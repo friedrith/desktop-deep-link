7cf21cbf578931dd2aa3b928dbe25184549ef080
Thibault Friedrich <thibault.friedrich@gmail.com>
2022-01-17 22:13:04 -0500

    fix: excludes option was including instead

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,14 @@ const defaultModule = {
 const allModules = [...modules, defaultModule]
 
 export interface Options {
-  excludes: Array<string>
+  excludes?: Array<string>
 }
 
 export const getListOfModules = () => allModules.map((m) => m.id)
 
 export default (uri: string, options: Options) => {
   return allModules
-    .filter((m: Module) => options.excludes.includes(m.id))
+    .filter((m: Module) => !(options?.excludes || []).includes(m.id))
     .find((m: Module) => m.match(uri))
     ?.transform(uri)
 }
